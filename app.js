fb11adf43d15e078edd9ad81dd6761ef6487c50e
Leonardo <leonardo@ITB001129D.itbam.org.br>
2022-08-23 16:30:54 -0400

    feat: change return of message to json

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello fucking world!'));
+app.get('/', (req, res) => res.json({
+    message: 'full pipeline test for approvement'
+}));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
